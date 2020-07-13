14b593de99faf5569be0c06ee2ca45584c7b32e2
dexpro988 <dejan.prodanovic@panonit.com>
2020-07-13 13:00:05 -0700

    Add MenagerMain window

--- DAN_XVL_Dejan_Prodanovic/DAN_XVL_Dejan_Prodanovic/Validation/ValidationClass.cs
+++ DAN_XVL_Dejan_Prodanovic/DAN_XVL_Dejan_Prodanovic/Validation/ValidationClass.cs
@@ -8,5 +8,18 @@ namespace DAN_XVL_Dejan_Prodanovic.Validation
 {
     class ValidationClass
     {
+        public static bool IsProductCodeValid(string code)
+        {
+            if (code.Length != 7)
+                return false;
+
+
+            for (int i = 0; i < code.Length; i++)
+            {
+                if (!Char.IsNumber(code, i))
+                    return false;
+            }
+            return true;
+        }
     }
 }
